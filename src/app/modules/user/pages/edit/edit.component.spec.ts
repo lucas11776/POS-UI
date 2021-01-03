14170a1dc9db38a9950796dc5c768ca6712f8393
ThembaLucasNgubeni <thembangubeni04@gmail.com>
2021-01-03 16:12:20 +0200

    Added upload user profile picture feature.

--- src/app/modules/user/pages/edit/edit.component.spec.ts
+++ src/app/modules/user/pages/edit/edit.component.spec.ts
@@ -1,14 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
+import { of, throwError } from 'rxjs';
 
 import { EditComponent } from './edit.component';
+import { UserService } from '../../shared/user.service';
 import { UserModule } from '../../user.module';
 import { SharedModule } from '../../../../shared/shared.module';
+import { _File as FileMock } from '../../../../core/mocks/file.mock';
+import { Profile as ProfileMock } from '../../../../core/mocks/user.mock';
 
 describe('EditComponent', () => {
   let component: EditComponent;
   let fixture: ComponentFixture<EditComponent>;
+  let ngxSpinnerService: NgxSpinnerService;
+  let userService: UserService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,6 +27,7 @@ describe('EditComponent', () => {
         SharedModule,
         RouterTestingModule,
         HttpClientTestingModule,
+        NgxSpinnerModule,
       ]
     })
     .compileComponents();
@@ -28,10 +36,50 @@ describe('EditComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(EditComponent);
     component = fixture.componentInstance;
+    ngxSpinnerService = TestBed.inject(NgxSpinnerService);
+    userService = TestBed.inject(UserService);
     fixture.detectChanges();
+  });
+
+  beforeEach(() => {
+    spyOn(ngxSpinnerService, 'show').and.returnValue();
+    spyOn(ngxSpinnerService, 'hide').and.returnValue();
   });
 
   it('should check if Edit component is created.', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should check UploadProfilePicture error when making the request to upload profile picture.', () => {
+    component.uploadProfilePictureError = { message: 'Something went wrong' };
+    component.uploadProfilePicture({ image: FileMock('img.png', 'image/png') });
+    expect(component.uploadProfilePictureError).toBeNull();
+  });
+
+  it('should display spinner when making required to upload profile picture.', () => {
+    component.uploadProfilePicture({ image: FileMock('img.png', 'image/png') });
+    expect(ngxSpinnerService.show).toHaveBeenCalled();
+  });
+
+  it('should close spinner after profile picture was uploaded successfully.', fakeAsync(() => {
+    spyOn(userService, 'uploadProfilePicture').and.returnValue(of(ProfileMock()));
+    component.uploadProfilePicture({ image: FileMock('img.png', 'image/png') });
+    tick();
+    expect(ngxSpinnerService.hide).toHaveBeenCalled();
+  }));
+
+  it('should assign error to local variable if upload profile picture request failed.', fakeAsync(() => {
+    let error = { message: 'Something went wrong' };
+    spyOn(userService, 'uploadProfilePicture').and.returnValue(throwError(error));
+    component.uploadProfilePicture({ image: FileMock('img.png', 'image/png') });
+    tick();
+    expect(component.uploadProfilePictureError).toEqual(error);
+  }));
+
+  it('should close spinner when upload profile picture request failed.', fakeAsync(() => {
+    spyOn(userService, 'uploadProfilePicture').and.returnValue(throwError({ message: 'Something went wrong.' }));
+    component.uploadProfilePicture({ image: FileMock('img.png', 'image/png') });
+    tick();
+    expect(ngxSpinnerService.hide).toHaveBeenCalled();
+  }));
 });
--- src/app/modules/user/pages/edit/edit.component.ts
+++ src/app/modules/user/pages/edit/edit.component.ts
@@ -1,14 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { SubSink } from 'subsink';
+
+import { UserService } from '../../shared/user.service';
+import { Profile, UploadProfilePicture } from '../../../../shared/models/user.model';
+import { Error } from '../../../../shared/models/api.model';
 
 @Component({
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit {
+export class EditComponent implements OnInit, OnDestroy {
+  updatePersonalDetailsError: Error;
+  uploadProfilePictureError: Error;
+  updateDescriptionError: Error;
+  updateAddressError: Error;
+  sub = new SubSink;
 
-  constructor() { }
+  constructor(
+    private _ngxSpinnerService: NgxSpinnerService,
+    private _userService: UserService) { }
 
   ngOnInit(): void {
   }
 
+  uploadProfilePicture(event$: UploadProfilePicture): void {
+    this.uploadProfilePictureError = null;
+    this._ngxSpinnerService.show();
+    this.sub.sink = this._userService.uploadProfilePicture(event$)
+      .subscribe(
+        profile => this.uploadProfilePictureSuccess(profile),
+        error => this.uploadProfilePictureFailed(error));
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  protected uploadProfilePictureSuccess(profile: Profile): void {
+    this._ngxSpinnerService.hide();
+  }
+
+  protected uploadProfilePictureFailed(error: Error): void {
+    this.uploadProfilePictureError = error;
+    this._ngxSpinnerService.hide();
+  }
 }
